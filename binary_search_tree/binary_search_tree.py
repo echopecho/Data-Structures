9f60f376174ce14832e4028962478f10ab1cdc2e
Phil Smith <pbsmithtbn@gmail.com>
2019-09-10 21:55:21 -0500

    Complete DLL and BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,57 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        if self.value == self.left == self.right == None:
+            self.value = value
+        elif value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        elif target > self.value and self.right:
+            return self.right.contains(target)
+        return False
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        if not hasattr(self.right, "value"):
+            return self.value
+        else:
+            return self.right.get_max()
 
-  def for_each(self, cb):
-    pass
+    def for_each(self, cb):
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
+
+
+test = BinarySearchTree(4)
+test.insert(3)
+test.insert(5)
+test.insert(1)
+test.insert(10)
+print(test.contains(-11))
+print(test.get_max())
+
+
+def test_func(value):
+    print(value + 4)
+
+
+test.for_each(test_func)
